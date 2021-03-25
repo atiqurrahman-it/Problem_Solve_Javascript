bcdcf62ce0ca8f264af3bee9a0ec3a2242b7eaa4
ATIQUR RAHMAN <75171760+atiqurrahman-it@users.noreply.github.com>
2021-03-26 00:40:31 +0600

    problem_1004

---
+++ URI/problem_1004.js
@@ -0,0 +1,7 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var a=Number(lines[0]);
+var b=Number(lines[1]);
+var result=a*b;
+console.log(`PROD = ${result}`)
