ec10ea34fe830dbc23236827b458a2da41695bdf
ATIQUR RAHMAN <75171760+atiqurrahman-it@users.noreply.github.com>
2021-03-26 15:26:28 +0600

    problem_1000

---
+++ URI/problem.js
@@ -0,0 +1,21 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+// var [a,b]= lines.map(Number);
+
+// var [a,b]=lines.shift().split(" ").map(item=> parseInt(item));
+// var [A,B]=input.split(" ").map(item=> parseFloat(item));
+
+/*
+var a=parseFloat(lines.shift());
+var b=parseFloat(lines.shift());
+*/
+
+/*
+var a=Number(lines[0]);
+var b=Number(lines[1]);
+*/
+
+
+console.log(a);
+console.log(b);
