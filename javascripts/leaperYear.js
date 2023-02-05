959ec2ddd327220552f630228527a2a129564cd7
atiqur <atiqurrahman.cse2552@gmail.com>
2023-02-06 00:12:35 +0600

    update

--- javascripts/leaperYear.js
+++ javascripts/leaperYear.js
@@ -1,24 +1,51 @@
 
-function findLeapYear(arrOfYears){
-//write your code here
-  let leapYear=[]
+// function findLeapYear(arrOfYears){
+// //write your code here
+//   let leapYear=[]
   
-  // calculate leaper 
-  function FindLeaper(year){
-      if ((year%4 ==0 && year%100 !=0) || (year%400==0)){
-          return leapYear.push(year)
-      }
-  }
+//   // way one 
+// //   function FindLeaper(year){
+// //       if ((year%4 ==0 && year%100 !=0) || (year%400==0)){
+// //         leapYear.push(year)
+// //       }
+// //   }
   
-  for(let i=0;i<arrOfYears.length;i++){
-      FindLeaper(arrOfYears[i])
-  }
-  
-  return leapYear
-  
-//don't forget to write return
-}
+// //   for(let i=0;i<arrOfYears.length;i++){
+// //       FindLeaper(arrOfYears[i])
+// //   }
 
+//   // way two 
+//   for(let year of years){
+//     if ((year%4 ==0 && year%100 !=0) || (year%400==0)){
+//         leapYear.push(year)
+//       }
+//   }
+
+  
+//   return leapYear
+  
+// //don't forget to write return
+// }
+
+// let years=[2023,2024,2025,2028,2030]
+// let result=findLeapYear(years)
+// console.log(result)
+
+
+
+
+
+function findLeapYear(arrOfYears){
+      let leapYear=[]
+      for(let year of arrOfYears){
+        if ((year%4 ==0 && year%100 !=0) || (year%400==0)){
+            leapYear.push(year)
+          }
+      }
+      return leapYear
+    }
+    
 let years=[2023,2024,2025,2028,2030]
 let result=findLeapYear(years)
 console.log(result)
+
